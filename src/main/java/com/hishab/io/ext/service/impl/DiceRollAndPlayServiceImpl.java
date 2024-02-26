e20872bcab82f79bcb94c37d3b538dc975d05836
shyfuzzaman <mdshyfuzzaman@gmai.com>
2024-02-26 13:28:11 +0600

    added async method

--- src/main/java/com/hishab/io/ext/service/impl/DiceRollAndPlayServiceImpl.java
+++ src/main/java/com/hishab/io/ext/service/impl/DiceRollAndPlayServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -62,6 +63,7 @@ public class DiceRollAndPlayServiceImpl implements DiceRollAndPlayService {
      * @param players the players
      */
     @Override
+    @Async
     public void rollDice(Map<String, Player> players) {
         rollAndPlay(players);
     }
